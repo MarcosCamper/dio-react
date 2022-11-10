fc29453d1d53ceda60068da9f5f0ea277c8e273e
MarcosCamper <marcosmgfl@gmail.com>
2022-11-10 10:58:29 -0300

    adicionado formulario na pag de login com validacao

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { Controller } from "react-hook-form";
 import {
     IconContainer,
+    ErrorText,
     InputContainer,
     InputText,
 } from './styles';
-const Input = ({leftIcon, name, ...rest}) => {
+const Input = ({leftIcon, errorMessage, name, control, ...rest}) => {
   return (
+    <>
     <InputContainer>
         {leftIcon ? (<IconContainer>{leftIcon}</IconContainer> ) : null}
-        <InputText {...rest} />
+        
+        <Controller
+          name={name}
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => <InputText {...rest} {...field} />}
+      />
     </InputContainer>
+    {errorMessage ? <ErrorText>{errorMessage}</ErrorText> : null}
+    </>
   )
 }
 
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,17 +1,9 @@
 import { MdEmail, MdPassword } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
-import {
-    Column,
-    Container,
-    CriarText,
-    EsqueciText, 
-    Row, 
-    SubTitleLogin, 
-    Title, 
-    TitleLogin, 
-    Wrapper
-
-} from './styles'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import {    Column,  Container, CriarText, EsqueciText, Row, SubTitleLogin, Title, TitleLogin, Wrapper} from './styles'
 
 
 import { Input } from "../../components/Input";
@@ -19,9 +11,22 @@ import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 
 
-
+const schema = yup.object({
+    email: yup.string().email('Email não é válido').required('Campo obrigatório'),
+    password: yup.string().min(3, 'No minimo 3 caracteres').required('Campo obrigatório'),
+  }).required();
 
 const Login = () => {
+
+    const { control, handleSubmit, formState: { errors, isValid } } = useForm({
+        resolver: yupResolver(schema),
+        mode: 'onChange',
+
+    });
+
+    console.log(isValid, errors);
+
+    const onSubmit = data => console.log(data);
 
     const navigate = useNavigate();
     const handleClickSignIn = () => {
@@ -41,10 +46,10 @@ const Login = () => {
             <Wrapper>
                 <TitleLogin>Faca seu Cadastro</TitleLogin>
                 <SubTitleLogin>Faca seu Login e make the change._</SubTitleLogin>
-                <form>
-                    <Input placeholder="Email" leftIcon={<MdEmail/>}/>
-                    <Input placeholder="Senha" type="password" leftIcon={<MdPassword/>}/>
-                    <Button title="Entrar" variant="secundary" onClick={handleClickSignIn} type="button"/>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input name="email" errorMessage={errors?.email?.message} control={control} placeholder="Email" leftIcon={<MdEmail/>}/>
+                    <Input name="password" errorMessage={errors?.password?.message} control={control} placeholder="Senha" type="password" leftIcon={<MdPassword/>}/>
+                    <Button title="Entrar" variant="secundary" type="submit"/>
                 </form>
                 <Row>
                     <EsqueciText>Esqueci minha senha</EsqueciText>
